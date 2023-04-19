8114b28adcf3e945fc4d06116869e06186b91419
ATran2000 <89952087+ATran2000@users.noreply.github.com>
2023-04-19 02:00:55 -0400

    Update AnimeList.tsx

--- components/AnimeList.tsx
+++ components/AnimeList.tsx
@@ -12,7 +12,7 @@ export default function AnimeList({ data, title }: AnimeListProps) {
     }
 
     return (
-        <div className="px-4 md:px-12 mt-4 space-y-8">
+        <div className="px-4 md:px-24 mt-4 space-y-8">
             <div>
                 <p className="text-white text-md md:text-xl lg:text-2xl font semibold mb-4">
                     {title}
