777cd6a22cad33442a3d73f8daab4d06b521b7bf
ATran2000 <89952087+ATran2000@users.noreply.github.com>
2023-04-18 22:39:10 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           content="Check out my favorite anime!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:image:secure_url" content="https://alvins-donut-capybara.vercel.app/images/profile.jpg" />
+        <meta property="og:image:secure_url" content="https://alvins-myflix.vercel.app/images/profile.jpg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className="text-white px-2 py-2">myFlix!</h1>
