6406d4231e212f518eac49b969ed6d2ff63d0f35
ATran2000 <89952087+ATran2000@users.noreply.github.com>
2023-04-20 22:56:24 -0400

    Update About.tsx

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,8 +1,28 @@
+import Head from 'next/head'
 import Navbar from '@/components/Navbar'
 
 export default function About() {
     return (
         <div>
+            <Head>
+                <title>myFlix</title>
+                <meta property="og:url" content="https://alvins-myflix.vercel.app/" />
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:title"
+                    content="myFlix"
+                />
+                <meta
+                    property="og:description"
+                    content="Check out my favorite anime!"
+                />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta property="og:image:secure_url" content="https://alvins-myflix.vercel.app/images/profile.jpg" />
+                <meta property="og:image:width" content="500" />
+                <meta property="og:image:height" content="500" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <Navbar />
         </div>
     )
