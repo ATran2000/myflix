cdccdc4fa32e25560f9961e94afcb317a59f18b9
ATran2000 <89952087+ATran2000@users.noreply.github.com>
2023-04-25 02:56:08 -0400

    Updated RandomRecommendation component so that it is easier to click the button

--- components/RandomRecommendation.tsx
+++ components/RandomRecommendation.tsx
@@ -12,8 +12,8 @@ export default function RandomRecommendation() {
     }, [revalidateData]);
 
     return (
-        <div className="flex justify-center items-center">
-            <div onClick={handleClick} className="text-center font-mono text-green-400 cursor-pointer hover:text-green-500 transition mb-8">
+        <div className="flex justify-center items-center m-auto w-80 mb-6">
+            <div onClick={handleClick} className="text-center font-mono text-green-400 cursor-pointer hover:text-green-500 transition w-full pt-4 pl-2 pr-2">
                 <h1>Click for Random Recommendation</h1>
             </div>
         </div>
