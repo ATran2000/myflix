0daea86f170efa1cdc01ad168465647b8d469e42
ATran2000 <89952087+ATran2000@users.noreply.github.com>
2023-04-26 17:19:54 -0400

    Used TechnologiesTableRow component in my TechnologiesTable component
    All tr elements in the TechnologiesTable component were converted to use the TechnologiesTableRow component except for Lodash and Git because they used different image sizes

--- components/TechnologiesTable.tsx
+++ components/TechnologiesTable.tsx
@@ -14,165 +14,75 @@ export default function TechnologiesTable() {
                     </thead>
                     <tbody className="divide-y-2 divide-white-100">
                         <TechnologiesTableRow
-                            link="https://nextjs.org/"
+                            link={"https://nextjs.org/"}
                             technologyName={"NextJS"}
                             imageSource={"/images/nextjs_logo.svg"}
                             imageAlt={"NextJS Logo"}
                             description={"NextJS is an open-source React framework for web development. It is used for its component-based architecture, JSX syntax, automatic code splitting, pre-rendering capabilities, ease of routing, and typescript support."}
                         />
-                        <tr className="divide-x-2 divide-white">
-                            <td className="bg-white hover:opacity-90">
-                                <Link href="https://www.typescriptlang.org/" className="p-4 block w-full flex justify-center items-center">
-                                    <img className="h-12 w-20" src="/images/typescript_logo.svg" alt="TypeScript Logo" />
-                                </Link>
-                            </td>
-                            <td className="p-4">
-                                <Link href="https://www.typescriptlang.org/" >
-                                    <p className="text-white hover:underline">TypeScript</p>
-                                </Link>
-                            </td>
-                            <td className="p-4 overflow-y-scroll scrollbar">
-                                <p className="text-white break-words h-12 text-left text-xs md:text-base">
-                                    Stuff about typescript
-                                </p>
-                            </td>
-                        </tr>
-                        <tr className="divide-x-2 divide-white">
-                            <td className="bg-white hover:opacity-90">
-                                <Link href="https://tailwindcss.com/" className="p-4 block w-full flex justify-center items-center">
-                                    <img className="h-12 w-20" src="/images/tailwindcss_logo.svg" alt="TailwindCSS Logo" />
-                                </Link>
-                            </td>
-                            <td className="p-4">
-                                <Link href="https://tailwindcss.com/" >
-                                    <p className="text-white hover:underline">TailwindCSS</p>
-                                </Link>
-                            </td>
-                            <td className="p-4 overflow-y-scroll scrollbar">
-                                <p className="text-white break-words h-12 text-left text-xs md:text-base">
-                                    Stuff about tailwindcss
-                                </p>
-                            </td>
-                        </tr>
-                        <tr className="divide-x-2 divide-white">
-                            <td className="bg-white hover:opacity-90">
-                                <Link href="https://www.mongodb.com/" className="p-4 block w-full flex justify-center items-center">
-                                    <img className="h-12 w-20" src="/images/mongodb_logo.svg" alt="MongoDB Logo" />
-                                </Link>
-                            </td>
-                            <td className="p-4">
-                                <Link href="https://www.mongodb.com/" >
-                                    <p className="text-white hover:underline">MongoDB</p>
-                                </Link>
-                            </td>
-                            <td className="p-4 overflow-y-scroll scrollbar">
-                                <p className="text-white break-words h-12 text-left text-xs md:text-base">
-                                    Stuff about mongodb
-                                </p>
-                            </td>
-                        </tr>
-                        <tr className="divide-x-2 divide-white">
-                            <td className="bg-white hover:opacity-90">
-                                <Link href="https://www.prisma.io/" className="p-4 block w-full flex justify-center items-center">
-                                    <img className="h-12 w-20" src="/images/prisma_logo.svg" alt="Prisma Logo" />
-                                </Link>
-                            </td>
-                            <td className="p-4">
-                                <Link href="https://www.prisma.io/" >
-                                    <p className="text-white hover:underline">Prisma</p>
-                                </Link>
-                            </td>
-                            <td className="p-4 overflow-y-scroll scrollbar">
-                                <p className="text-white break-words h-12 text-left text-xs md:text-base">
-                                    Stuff about prisma
-                                </p>
-                            </td>
-                        </tr>
-                        <tr className="divide-x-2 divide-white">
-                            <td className="bg-white hover:opacity-90">
-                                <Link href="https://vercel.com/" className="p-4 block w-full flex justify-center items-center">
-                                    <img className="h-12 w-20" src="/images/vercel_logo.svg" alt="Vercel Logo" />
-                                </Link>
-                            </td>
-                            <td className="p-4">
-                                <Link href="https://vercel.com/" >
-                                    <p className="text-white hover:underline">Vercel</p>
-                                </Link>
-                            </td>
-                            <td className="p-4 overflow-y-scroll scrollbar">
-                                <p className="text-white break-words h-12 text-left text-xs md:text-base">
-                                    Stuff about vercel
-                                </p>
-                            </td>
-                        </tr>
-                        <tr className="divide-x-2 divide-white">
-                            <td className="bg-white hover:opacity-90">
-                                <Link href="https://docs.pmnd.rs/zustand/getting-started/introduction" className="p-4 block w-full flex justify-center items-center">
-                                    <img className="h-12 w-20" src="/images/zustand_logo.png" alt="Zustand Logo" />
-                                </Link>
-                            </td>
-                            <td className="p-4">
-                                <Link href="https://docs.pmnd.rs/zustand/getting-started/introduction" >
-                                    <p className="text-white hover:underline">Zustand</p>
-                                </Link>
-                            </td>
-                            <td className="p-4 overflow-y-scroll scrollbar">
-                                <p className="text-white break-words h-12 text-left text-xs md:text-base">
-                                    Stuff about zustand
-                                </p>
-                            </td>
-                        </tr>
-                        <tr className="divide-x-2 divide-white">
-                            <td className="bg-white hover:opacity-90">
-                                <Link href="https://swr.vercel.app/" className="p-4 block w-full flex justify-center items-center">
-                                    <img className="h-12 w-20" src="/images/swr_logo.png" alt="SWR Logo" />
-                                </Link>
-                            </td>
-                            <td className="p-4">
-                                <Link href="https://swr.vercel.app/" >
-                                    <p className="text-white hover:underline">SWR</p>
-                                </Link>
-                            </td>
-                            <td className="p-4 overflow-y-scroll scrollbar">
-                                <p className="text-white break-words h-12 text-left text-xs md:text-base">
-                                    Stuff about SWR
-                                </p>
-                            </td>
-                        </tr>
-                        <tr className="divide-x-2 divide-white">
-                            <td className="bg-white hover:opacity-90">
-                                <Link href="https://axios-http.com/" className="p-4 block w-full flex justify-center items-center">
-                                    <img className="h-12 w-20" src="/images/axios_logo.svg" alt="Axios Logo" />
-                                </Link>
-                            </td>
-                            <td className="p-4">
-                                <Link href="https://axios-http.com/" >
-                                    <p className="text-white hover:underline">Axios</p>
-                                </Link>
-                            </td>
-                            <td className="p-4 overflow-y-scroll scrollbar">
-                                <p className="text-white break-words h-12 text-left text-xs md:text-base">
-                                    Stuff about Axios
-                                </p>
-                            </td>
-                        </tr>
-                        <tr className="divide-x-2 divide-white">
-                            <td className="bg-white hover:opacity-90">
-                                <Link href="https://eslint.org/" className="p-4 block w-full flex justify-center items-center">
-                                    <img className="h-12 w-20" src="/images/eslint_logo.svg" alt="ESlint Logo" />
-                                </Link>
-                            </td>
-                            <td className="p-4">
-                                <Link href="https://eslint.org/" >
-                                    <p className="text-white hover:underline">ESLint</p>
-                                </Link>
-                            </td>
-                            <td className="p-4 overflow-y-scroll scrollbar">
-                                <p className="text-white break-words h-12 text-left text-xs md:text-base">
-                                    Stuff about ESLint
-                                </p>
-                            </td>
-                        </tr>
+                        <TechnologiesTableRow
+                            link={"https://www.typescriptlang.org/"}
+                            technologyName={"TypeScript"}
+                            imageSource={"/images/typescript_logo.svg"}
+                            imageAlt={"TypeScript Logo"}
+                            description={"Stuff about typescript"}
+                        />
+                        <TechnologiesTableRow
+                            link={"https://tailwindcss.com/"}
+                            technologyName={"TailwindCSS"}
+                            imageSource={"/images/tailwindcss_logo.svg"}
+                            imageAlt={"TailwindCSS Logo"}
+                            description={"Stuff about tailwindcss"}
+                        />
+                        <TechnologiesTableRow
+                            link={"https://www.mongodb.com/"}
+                            technologyName={"MongoDB"}
+                            imageSource={"/images/mongodb_logo.svg"}
+                            imageAlt={"MongoDB Logo"}
+                            description={"Stuff about mongodb"}
+                        />
+                        <TechnologiesTableRow
+                            link={"https://www.prisma.io/"}
+                            technologyName={"Prisma"}
+                            imageSource={"/images/prisma_logo.svg"}
+                            imageAlt={"Prisma Logo"}
+                            description={"Stuff about prisma"}
+                        />
+                        <TechnologiesTableRow
+                            link={"https://vercel.com/"}
+                            technologyName={"Vercel"}
+                            imageSource={"/images/vercel_logo.svg"}
+                            imageAlt={"Vercel Logo"}
+                            description={"Stuff about vercel"}
+                        />
+                        <TechnologiesTableRow
+                            link={"https://docs.pmnd.rs/zustand/getting-started/introduction"}
+                            technologyName={"Zustand"}
+                            imageSource={"/images/zustand_logo.png"}
+                            imageAlt={"Zustand Logo"}
+                            description={"Stuff about zustand"}
+                        />
+                        <TechnologiesTableRow
+                            link={"https://swr.vercel.app/"}
+                            technologyName={"SWR"}
+                            imageSource={"/images/swr_logo.png"}
+                            imageAlt={"SWR Logo"}
+                            description={"Stuff about SWR"}
+                        />
+                        <TechnologiesTableRow
+                            link={"https://axios-http.com/"}
+                            technologyName={"Axios"}
+                            imageSource={"/images/axios_logo.svg"}
+                            imageAlt={"Axios Logo"}
+                            description={"Stuff about Axios"}
+                        />
+                        <TechnologiesTableRow
+                            link={"https://eslint.org/"}
+                            technologyName={"ESLint"}
+                            imageSource={"/images/eslint_logo.svg"}
+                            imageAlt={"ESLint Logo"}
+                            description={"Stuff about ESLint"}
+                        />
                         <tr className="divide-x-2 divide-white">
                             <td className="bg-white hover:opacity-90">
                                 <Link href="https://lodash.com/" className="p-4 block w-full flex justify-center items-center">
@@ -207,23 +117,13 @@ export default function TechnologiesTable() {
                                 </p>
                             </td>
                         </tr>
-                        <tr className="divide-x-2 divide-white">
-                            <td className="bg-white hover:opacity-90">
-                                <Link href="https://github.com/" className="p-4 block w-full flex justify-center items-center">
-                                    <img className="h-12 w-20" src="/images/github_logo.png" alt="Github Logo" />
-                                </Link>
-                            </td>
-                            <td className="p-4">
-                                <Link href="https://github.com/" >
-                                    <p className="text-white hover:underline">Github</p>
-                                </Link>
-                            </td>
-                            <td className="p-4 overflow-y-scroll scrollbar">
-                                <p className="text-white break-words h-12 text-left text-xs md:text-base">
-                                    Stuff about Github
-                                </p>
-                            </td>
-                        </tr>
+                        <TechnologiesTableRow
+                            link={"https://github.com/"}
+                            technologyName={"Github"}
+                            imageSource={"/images/github_logo.png"}
+                            imageAlt={"Github Logo"}
+                            description={"Stuff about Github"}
+                        />
                     </tbody>
                 </table>
             </div>
